657bfcc2f34fc319b316113f09e90d4d8e056e92
IvanSlivenko <ivan8822@ukr.net>
2025-10-08 19:10:40 +0300

    08-10-2025-19-10

--- trello/src/app/board/[id]/page.tsx
+++ trello/src/app/board/[id]/page.tsx
@@ -24,14 +24,22 @@ export default async function BoardPage({ params }: PageProps) {
   }
 
   return (
-    <div className="container mx-auto">
+    <div
+      className="container mx-auto"
+      style={{
+        height: "calc(100% - 2.5rem - 4rem - 2.5rem - 2rem)",
+      }}
+    >
       {/* {JSON.stringify(board, null, 2)} */}
-      <h1 className="text-white text-4xl text-center">{board.title}</h1>
-
-      <div className="block p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          test title
-        </h5>
+      <h1 className="text-white text-4xl text-center mb-8">{board.title}</h1>
+      <div className="flex h-full">
+        <div className="block  h-full w-full p-4 border rounded-lg shadow-sm bg-gray-800 border-gray-700">
+          <div>
+            <h5 className="text-lg font-bold tracking-tight text-white">
+              Column name
+            </h5>
+          </div>
+        </div>
       </div>
     </div>
   );
