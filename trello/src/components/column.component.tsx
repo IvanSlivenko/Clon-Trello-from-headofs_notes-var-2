4e2521da258f625d7d92415f8439834a9cbf2b1e
IvanSlivenko <ivan8822@ukr.net>
2025-10-17 21:53:46 +0300

    17-10-2025-21-53

--- trello/src/components/column.component.tsx
+++ trello/src/components/column.component.tsx
@@ -1,22 +1,48 @@
+"use client";
+
 import { Columns } from "@prisma/client";
+import { useState, DragEvent } from "react";
 
 interface ColumnProps {
   column: Columns;
 }
 
 export function Column({ column }: ColumnProps) {
-  const onResizeStart = (e) => {
+  const [initialDraX, setInitialDragX] = useState(0);
+  const [width, setWidth] = useState(column.width);
+
+  const onResizeStart = (e: DragEvent<HTMLDivElement>) => {
     console.log("resize start", e);
+    setInitialDragX(e.clientX);
+  };
+
+  const onResize = (e: DragEvent<HTMLDivElement>) => {
+    console.log("resize");
+    const movedBy = e.clientX - initialDraX;
+    setInitialDragX(e.clientX);
+    setWidth(width + movedBy);
   };
 
   return (
     <div
       className="block h-full w-full p-4 border rounded-lg shadow-sm bg-gray-800 border-gray-700"
+      // style={{
+      //   // minWidth: column.width,
+      //   minWidth: width,
+      //   // width: column.width,
+      //   width: width,
+      //   //   width: 700,
+      //   //   minWidth: 700,
+      // }}
+
+      // style={{
+      //   minWidth: isFinite(column.width) ? `${column.width}px` : "200px", // дефолтне значення
+      // }}
+
       style={{
-        minWidth: column.width,
-        width: column.width,
-        //   width: 700,
-        //   minWidth: 700,
+        minWidth: `${width}px`,
+        // minWidth: `${column.width}px`,
+        width: `${width}px`,
       }}
     >
       <div className="relative">
@@ -27,6 +53,7 @@ export function Column({ column }: ColumnProps) {
           className="absolute right-0 top-0 "
           draggable
           onDragStart={onResizeStart}
+          onDrag={onResize}
         >
           move
         </div>
