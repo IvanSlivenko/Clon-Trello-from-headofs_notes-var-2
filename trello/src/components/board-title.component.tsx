6f41fc5c1a917ad7548c85ba663b5f25a9fb5a8a
IvanSlivenko <ivan8822@ukr.net>
2025-10-23 17:59:32 +0300

    23-10-2025-17-59

--- trello/src/components/board-title.component.tsx
+++ trello/src/components/board-title.component.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import { useBoardQuery } from "@/hooks/use-board-query";
-
 interface BoardTitleProps {
-  boardId: string;
+  title: string;
 }
 
-export function BoardTitle({ boardId }: BoardTitleProps) {
-  const { data } = useBoardQuery({ boardId });
-  return (
-    <h1 className="text-white text-4xl text-center mb-8">{data?.title}</h1>
-  );
+export function BoardTitle({ title }: BoardTitleProps) {
+  return <h1 className="text-white text-4xl text-center mb-8">{title}</h1>;
 }
