8a99240a52daf850a9e1819caefb525759135b51
IvanSlivenko <ivan8822@ukr.net>
2025-10-24 12:39:30 +0300

    24-10-2025-12-39

--- trello/src/components/board-title.component.tsx
+++ trello/src/components/board-title.component.tsx
@@ -1,14 +1,38 @@
 "use client";
 
+import clsx from "clsx";
+import { useRef, useState } from "react";
+import { twMerge } from "tailwind-merge";
+
 interface BoardTitleProps {
   title: string;
 }
 
 export function BoardTitle({ title }: BoardTitleProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const turnOnEditing = () => {
+    if (isEditing) {
+      return;
+    }
+    setIsEditing(true);
+  };
+
+  const titleRef = useRef<HTMLHeadElement>(null);
+
+  const titleClasses = clsx({
+    "cursor-pointer": !isEditing,
+    "cursor-text": isEditing,
+  });
+
   return (
     <h1
-      className="text-white text-4xl text-center mb-8 font-bold hover:bg-black/20 inline-block mx-auto"
-      contentEditable={false}
+      className={twMerge(
+        "text-white text-4xl text-center mb-8 font-bold transition outline-none  hover:bg-black/20",
+        titleClasses
+      )}
+      contentEditable={isEditing}
+      onClick={turnOnEditing}
+      ref={titleRef}
     >
       {title}
     </h1>
